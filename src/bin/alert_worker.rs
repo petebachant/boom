eea8ee33a6ce767a5b5fe39ce0ddea642bb37d81
Theodlz <theophile.dulaz@gmail.com>
2024-08-14 15:56:19 -0700

    add config.default.yaml

--- src/bin/alert_worker.rs
+++ src/bin/alert_worker.rs
@@ -1,14 +1,28 @@
 use redis::AsyncCommands;
+use std::env;
 
 use boom::conf;
 use boom::alert;
 
 #[tokio::main]
 async fn main() {
-    let config_file = conf::load_config("./config.yaml").unwrap();    
+    let args: Vec<String> = env::args().collect();
+    // user can pass the path to a config file, but it is optional.
+    // if not provided, we use the default config.default.yaml
+    let config_file = if args.len() > 1 {
+        conf::load_config(&args[1]).unwrap()
+    } else {
+        println!("No config file provided, using default config.default.yaml");
+        conf::load_config("./config.default.yaml").unwrap()
+    };
 
     let xmatch_configs = conf::build_xmatch_configs(&config_file);
-    let db = conf::build_db(&config_file).await;
+    let db: mongodb::Database = conf::build_db(&config_file).await;
+
+    if let Err(e) = db.list_collection_names().await {
+        println!("Error connecting to the database: {}", e);
+        return;
+    }
 
     let client_redis = redis::Client::open(
         "redis://localhost:6379".to_string()
