5119b70a82ffa27316956b822d0cd77f1b29dbca
Theodlz <theophile.dulaz@gmail.com>
2025-03-27 14:46:11 -0700

    after processing alerts, query them from Mongodb to verify that some key fields are present

--- tests/test_lsst.rs
+++ tests/test_lsst.rs
@@ -1,7 +1,9 @@
 use boom::{
     alert::{AlertWorker, LsstAlertWorker},
+    conf,
     utils::testing::drop_alert_from_collections,
 };
+use mongodb::bson::doc;
 
 const CONFIG_FILE: &str = "tests/config.test.yaml";
 
@@ -93,4 +95,56 @@ async fn test_process_lsst_alert() {
     let result = alert_worker.process_alert(&bytes_content).await;
 
     assert!(result.is_err());
+
+    // let's query the database to check if the alert was inserted
+    let config_file = conf::load_config(CONFIG_FILE).unwrap();
+    let db = conf::build_db(&config_file).await.unwrap();
+    let alert_collection_name = "LSST_alerts";
+    let filter = doc! {"_id": 25409136044802067_i64};
+
+    let alert = db
+        .collection::<mongodb::bson::Document>(alert_collection_name)
+        .find_one(filter.clone())
+        .await
+        .unwrap();
+    assert!(alert.is_some());
+    let alert = alert.unwrap();
+    assert_eq!(alert.get_i64("_id").unwrap(), 25409136044802067);
+    assert_eq!(alert.get_i64("objectId").unwrap(), 25401295582003262);
+
+    // check that the cutouts were inserted
+    let cutout_collection_name = "LSST_alerts_cutouts";
+    let cutouts = db
+        .collection::<mongodb::bson::Document>(cutout_collection_name)
+        .find_one(filter.clone())
+        .await
+        .unwrap();
+    assert!(cutouts.is_some());
+    let cutouts = cutouts.unwrap();
+    assert_eq!(cutouts.get_i64("_id").unwrap(), 25409136044802067);
+    assert!(cutouts.contains_key("cutoutScience"));
+    assert!(cutouts.contains_key("cutoutTemplate"));
+    assert!(cutouts.contains_key("cutoutDifference"));
+
+    // check that the aux collection was inserted
+    let aux_collection_name = "LSST_alerts_aux";
+    let filter_aux = doc! {"_id": 25401295582003262_i64};
+    let aux = db
+        .collection::<mongodb::bson::Document>(aux_collection_name)
+        .find_one(filter_aux.clone())
+        .await
+        .unwrap();
+
+    assert!(aux.is_some());
+    let aux = aux.unwrap();
+    assert_eq!(aux.get_i64("_id").unwrap(), 25401295582003262);
+    // check that we have the arrays prv_candidates, prv_nondetections and fp_hists
+    let prv_candidates = aux.get_array("prv_candidates").unwrap();
+    assert_eq!(prv_candidates.len(), 3);
+
+    let prv_nondetections = aux.get_array("prv_nondetections").unwrap();
+    assert_eq!(prv_nondetections.len(), 0);
+
+    let fp_hists = aux.get_array("fp_hists").unwrap();
+    assert_eq!(fp_hists.len(), 3);
 }
