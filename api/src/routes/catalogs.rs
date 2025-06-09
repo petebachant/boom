e15ce30ada6f3115b8e4b70963c5a56b325351fc
Pete Bachant <petebachant@gmail.com>
2025-06-09 12:51:50 -0700

    Add API endpoint to perform a cone search query

--- api/src/routes/catalogs.rs
+++ api/src/routes/catalogs.rs
@@ -2,7 +2,10 @@ use crate::models::response;
 
 use actix_web::{HttpResponse, get, post, web};
 use futures::StreamExt;
+use futures::TryStreamExt;
 use mongodb::{Database, bson::doc};
+use std::collections::HashMap;
+use std::fmt;
 
 /// Helper function to add a prefix to a catalog name to get its collection name
 fn get_collection_name(catalog_name: &str) -> String {
@@ -301,4 +304,141 @@ pub async fn post_catalog_find_query(
         }
         Err(e) => response::internal_error(&format!("Error finding documents: {:?}", e)),
     }
+}
+
+#[derive(serde::Deserialize, Clone)]
+pub enum Unit {
+    Degrees,
+    Radians,
+    Arcseconds,
+    Arcminutes,
+}
+
+impl fmt::Debug for Unit {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Unit::Degrees => {
+                write!(f, "{}", "Degrees")
+            }
+            Unit::Radians => {
+                write!(f, "{}", "Radians")
+            }
+            Unit::Arcseconds => {
+                write!(f, "{}", "Arcseconds")
+            }
+            Unit::Arcminutes => {
+                write!(f, "{}", "Arcminutes")
+            }
+        }
+    }
+}
+
+#[derive(serde::Deserialize, Clone)]
+struct ConeSearchQuery {
+    filter: mongodb::bson::Document,
+    projection: Option<mongodb::bson::Document>,
+    radius: f64,
+    unit: Unit,
+    object_coordinates: HashMap<String, [f64; 2]>, // Map of catalog name to coordinates [RA, Dec]
+    limit: Option<i64>,
+    skip: Option<u64>,
+    sort: Option<mongodb::bson::Document>,
+    max_time_ms: Option<u64>,
+}
+
+impl ConeSearchQuery {
+    /// Convert to MongoDB Find options
+    fn to_find_options(&self) -> mongodb::options::FindOptions {
+        let mut options = mongodb::options::FindOptions::default();
+        if let Some(projection) = &self.projection {
+            options.projection = Some(projection.clone());
+        }
+        if let Some(limit) = self.limit {
+            options.limit = Some(limit);
+        }
+        if let Some(skip) = self.skip {
+            options.skip = Some(skip);
+        }
+        if let Some(sort) = &self.sort {
+            options.sort = Some(sort.clone());
+        }
+        if let Some(max_time_ms) = self.max_time_ms {
+            options.max_time = Some(std::time::Duration::from_millis(max_time_ms));
+        }
+        options
+    }
+}
+
+/// Post a query to do a cone search on a data catalog
+#[post("/catalogs/{catalog_name}/queries/cone-search")]
+pub async fn post_catalog_cone_search_query(
+    db: web::Data<Database>,
+    catalog_name: web::Path<String>,
+    body: web::Json<ConeSearchQuery>,
+) -> HttpResponse {
+    // Validate catalog name
+    if catalog_name.is_empty() {
+        return response::bad_request("Catalog name cannot be empty");
+    }
+    let catalog_name = catalog_name.into_inner();
+    // Check that there is a collection with this catalog name
+    let collection_names = match db.list_collection_names().await {
+        Ok(c) => c,
+        Err(e) => {
+            return response::internal_error(&format!("Error getting catalog info: {:?}", e));
+        }
+    };
+    let collection_name = get_collection_name(&catalog_name);
+    if !collection_names.contains(&collection_name) {
+        return response::bad_request(&format!("Catalog '{}' does not exist", catalog_name));
+    }
+    // Get the collection
+    let collection = db.collection::<mongodb::bson::Document>(&collection_name);
+    // Perform cone search over each set of object coordinates
+    let find_options = body.to_find_options();
+    let mut radius = body.radius;
+    let unit = body.unit.clone();
+    // Convert radius to radians based on unit
+    match unit {
+        Unit::Degrees => radius = radius.to_radians(),
+        Unit::Arcseconds => radius = radius.to_radians() / 3600.0,
+        Unit::Arcminutes => radius = radius.to_radians() / 60.0,
+        Unit::Radians => {}
+    }
+    let object_coordinates = &body.object_coordinates;
+    let mut docs: HashMap<String, Vec<mongodb::bson::Document>> = HashMap::new();
+    for (object_name, radec) in object_coordinates {
+        let mut filter = body.filter.clone();
+        let ra = radec[0] - 180.0;
+        let dec = radec[1];
+        let center_sphere = doc! {
+            "$centerSphere": [[ra, dec], radius]
+        };
+        let geo_within = doc! {
+            "$geoWithin": center_sphere
+        };
+        filter.insert("coordinates.radec_geojson", geo_within);
+        let cursor = match collection
+            .find(filter)
+            .with_options(find_options.clone())
+            .await
+        {
+            Ok(c) => c,
+            Err(e) => {
+                return response::internal_error(&format!("Error finding documents: {:?}", e));
+            }
+        };
+        // Create map entry for this object's cone search
+        let data = match cursor.try_collect::<Vec<mongodb::bson::Document>>().await {
+            Ok(d) => d,
+            Err(e) => {
+                return response::internal_error(&format!("Error collecting documents: {:?}", e));
+            }
+        };
+        docs.insert(object_name.clone(), data);
+    }
+    return response::ok(
+        &format!("Cone Search on {} completed", catalog_name),
+        serde_json::json!(docs),
+    );
 }
